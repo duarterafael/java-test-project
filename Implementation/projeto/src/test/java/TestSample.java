1ecc79d63847ee303f96030892c4fea8796b9008
AulaGitTR <aulagittr@gmail.com>
2024-01-24 19:19:57 -0300

    first commit

---
+++ Implementation/projeto/src/test/java/TestSample.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestSample {
+	@Test
+	public void testThatFails() {
+		assertTrue("This should fail", false);
+	}
+
+}
